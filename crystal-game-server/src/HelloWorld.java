80ad562ed8747d70871355d654daa5324d7c1e70
Balazs Pete <bazsip@gmail.com>
2014-01-30 17:40:14 +0000

    Add CrystalGameServer

---
+++ crystal-game-server/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * This is a Sample HelloWorld program
+ * @author Balazs Pete
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
