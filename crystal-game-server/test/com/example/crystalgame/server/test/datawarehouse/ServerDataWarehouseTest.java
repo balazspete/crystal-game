2a0f1ef8726ea409f4688950a37f8991da6f2eb1
Balazs Pete <bazsip@gmail.com>
2014-02-21 15:28:11 +0000

    Update ServerDataWarehouseTest

--- crystal-game-server/test/com/example/crystalgame/server/test/datawarehouse/ServerDataWarehouseTest.java
+++ crystal-game-server/test/com/example/crystalgame/server/test/datawarehouse/ServerDataWarehouseTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.example.crystalgame.library.data.HasID;
+import com.example.crystalgame.library.datawarehouse.DataWarehouseException;
 import com.example.crystalgame.library.util.RandomID;
 import com.example.crystalgame.server.datawarehouse.ServerDataWarehouse;
 
@@ -46,26 +47,39 @@ public class ServerDataWarehouseTest {
 	@Test
 	public void testPut() {
 		StringWithID obj = new StringWithID("test test");
-		assertNull("Data Warehouse 1 should be empty", dw1.get(StringWithID.class, obj.getID()));
-		assertTrue("Put should be successful", dw1.put(StringWithID.class, obj));
+		try {
+			assertNull("Data Warehouse 1 should be empty", dw1.get(StringWithID.class, obj.getID()));
+			assertTrue("Put should be successful", dw1.put(StringWithID.class, obj));
+		} catch (DataWarehouseException e) {
+			fail(e.getMessage());
+		}
 	}
 
 	@Test
 	public void testGet() {
 		StringWithID obj = new StringWithID("test test");
-		dw1.put(StringWithID.class, obj);
-		HasID _obj = dw1.get(StringWithID.class, obj.getID());
-		assertNotNull("Data Warehouse 1 should not retur null", _obj);
-		assertEquals("Returned object should have same ID", obj.getID(), _obj.getID());
-		assertEquals("Returned object should have same value", obj.getValue(), ((StringWithID) _obj).getValue());
+		try {
+			dw1.put(StringWithID.class, obj);
+			HasID _obj = dw1.get(StringWithID.class, obj.getID());
+			
+			assertNotNull("Data Warehouse 1 should not retur null", _obj);
+			assertEquals("Returned object should have same ID", obj.getID(), _obj.getID());
+			assertEquals("Returned object should have same value", obj.getValue(), ((StringWithID) _obj).getValue());
+		} catch (DataWarehouseException e) {
+			fail(e.getMessage());
+		}
 	}
 
 	@Test
 	public void testDelete() {
 		StringWithID obj = new StringWithID("test test");
-		dw1.put(StringWithID.class, obj);
 		
-		assertTrue("Data with key should not exist", dw1.delete(StringWithID.class, obj.getID()));
+		try {
+			dw1.put(StringWithID.class, obj);
+			assertTrue("Data with key should not exist", dw1.delete(StringWithID.class, obj.getID()));
+		} catch (DataWarehouseException e) {
+			fail(e.getMessage());
+		}
 	}
 
 	@Test
@@ -73,10 +87,14 @@ public class ServerDataWarehouseTest {
 		StringWithID obj = new StringWithID("test test");
 		StringWithID obj2 = new StringWithID("test test");
 		
-		dw1.put(StringWithID.class, obj);
-		dw1.put(StringWithID.class, obj2);
-		
-		assertEquals(2, dw1.getAll(StringWithID.class).size()); 
+		try {
+			dw1.put(StringWithID.class, obj);
+			dw1.put(StringWithID.class, obj2);
+			
+			assertEquals(2, dw1.getAll(StringWithID.class).size());
+		} catch (DataWarehouseException e) {
+			fail(e.getMessage());
+		}
 	}
 
 }
