e66d2e08da90c58dabb48df8b731e54e5d25e094
Balazs Pete <bazsip@gmail.com>
2014-03-06 17:49:53 +0000

    Add information concept

---
+++ crystal-game-library/src/com/example/crystalgame/library/data/Information.java
@@ -0,0 +1,50 @@
+package com.example.crystalgame.library.data;
+
+import java.io.Serializable;
+
+/**
+ * An object representing some general information
+ * @author Balazs Pete, Allen Thomas Varghese
+ *
+ */
+public class Information implements HasID {
+
+	private static transient final String PREKEY = "com.example.crystalgame.library.data.information.";
+	
+	private String key;
+	private Serializable value;
+	
+	/**
+	 * Create an information
+	 * @param key The key
+	 * @param value The value
+	 */
+	public Information(String key, Serializable value) {
+		this.key = PREKEY + key;
+		this.value = value;
+	}
+	
+	@Override
+	public String getID() {
+		return key;
+	}
+
+	/**
+	 * Get the value of the information
+	 * @return The information
+	 */
+	public Serializable getValue() {
+		return value;
+	}
+	
+	/*******************
+	 * PREDEFINED KEYS
+	 *******************/
+	
+	public static transient final String
+		GAME_NAME = "game_name",
+		GROUP_NAME = "group_name",
+		GROUP_MAX_PLAYERS = "group_max_players";
+	
+	
+}
